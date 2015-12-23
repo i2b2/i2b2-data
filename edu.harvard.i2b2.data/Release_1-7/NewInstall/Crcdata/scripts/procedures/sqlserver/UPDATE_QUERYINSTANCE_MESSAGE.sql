129bd34ea6fcc0217464cf539b5f8ca8b45781e9
Janice Donahoe <jdonahoe@partners.org>
2015-12-23 12:31:35 -0500

    CORE-201 Fixed issue with stored procedure not getting created

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata/scripts/procedures/sqlserver/UPDATE_QUERYINSTANCE_MESSAGE.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata/scripts/procedures/sqlserver/UPDATE_QUERYINSTANCE_MESSAGE.sql
@@ -1,23 +1,17 @@
-create procedure UPDATE_QUERYINSTANCE_MESSAGE (@message varchar(max), @instanceId int , @errorMsg varchar(max) = NULL OUTPUT) as 
+create procedure UPDATE_QUERYINSTANCE_MESSAGE (@message  varchar(MAX), @instanceId int , @errorMsg varchar(MAX) = NULL OUTPUT) as 
 begin 
-declare @ptrval binary(16), @i int
 
 BEGIN TRY
 BEGIN TRANSACTION
 
-select @ptrval = TEXTPTR(message), 
- @i=  datalength(message)
-from qt_query_instance
-where query_instance_id = @instanceId
- 
-updatetext qt_query_instance.message @ptrval @i 0 @message
+update qt_query_instance set message = @message where query_instance_id = @instanceId
 
  COMMIT
  END TRY 
  BEGIN CATCH
    if @@TRANCOUNT > 0 
       ROLLBACK
-   declare @errMsg nvarchar(4000), @errSeverity int
+   declare @errMsg nvarchar(MAX), @errSeverity int
    select @errMsg = ERROR_MESSAGE(), @errSeverity = ERROR_SEVERITY();
    set @errorMsg = @errMsg;
    RAISERROR(@errMsg,@errSeverity,1);
