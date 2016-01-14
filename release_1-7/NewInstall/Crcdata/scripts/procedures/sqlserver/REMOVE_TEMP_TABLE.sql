7a6a8ab17c34d6d71b5ba2f87a57205e908681cc
Janice Donahoe <jdonahoe@partners.org>
2016-01-14 08:26:11 -0500

    CORE-204 Removed duplicate files

--- release_1-7/NewInstall/Crcdata/scripts/procedures/sqlserver/REMOVE_TEMP_TABLE.sql
+++
@@ -1,7 +0,0 @@
-CREATE PROCEDURE REMOVE_TEMP_TABLE(@tempTableName VARCHAR(500)) 
-AS
-BEGIN 
-    declare @dropSql nvarchar(MAX);
-	set @dropSql = 'drop table ' + @tempTableName + ' ';
-	exec sp_executesql @dropSql;
-END;
--- release_1-7/NewInstall/Hivedata/scripts/im_db_lookup_sqlserver_insert_data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO IM_DB_LOOKUP(c_domain_id, c_project_path, c_owner_id, c_db_fullschema, c_db_datasource, c_db_servertype, c_db_nicename, c_db_tooltip, c_comment, c_entry_date, c_change_date, c_status_cd)
-  VALUES('i2b2demo', 'Demo/', '@', 'i2b2imdata.dbo', 'java:/IMDemoDS', 'SQLSERVER', 'IM', NULL, NULL, NULL, NULL, NULL);
