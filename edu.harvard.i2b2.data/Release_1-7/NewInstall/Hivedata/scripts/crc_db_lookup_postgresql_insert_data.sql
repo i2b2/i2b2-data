26a63aab1067a6ff8b63b91c2660cbca7a532f6c
mem61 <mem61@mikes-mbp.partners.org>
2019-05-22 15:27:45 -0400

    CORE_338 Added ACT to db lookup and fixed demographics

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Hivedata/scripts/crc_db_lookup_postgresql_insert_data.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Hivedata/scripts/crc_db_lookup_postgresql_insert_data.sql
@@ -1,4 +1,6 @@
 INSERT INTO CRC_DB_LOOKUP(c_domain_id, c_project_path, c_owner_id, c_db_fullschema, c_db_datasource, c_db_servertype, c_db_nicename, c_db_tooltip, c_comment, c_entry_date, c_change_date, c_status_cd)
   VALUES('i2b2demo', '/Demo/', '@', 'public', 'java:/QueryToolDemoDS', 'POSTGRESQL', 'Demo', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO CRC_DB_LOOKUP(c_domain_id, c_project_path, c_owner_id, c_db_fullschema, c_db_datasource, c_db_servertype, c_db_nicename, c_db_tooltip, c_comment, c_entry_date, c_change_date, c_status_cd)
+  VALUES('i2b2demo', '/ACT/', '@', 'public', 'java:/QueryToolDemoDS', 'POSTGRESQL', 'Demo', NULL, NULL, NULL, NULL, NULL);
 
 
--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Hivedata/scripts/ont_db_lookup_oracle_insert_data.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Hivedata/scripts/ont_db_lookup_oracle_insert_data.sql
@@ -1,2 +1,5 @@
 INSERT INTO ONT_DB_LOOKUP(c_domain_id, c_project_path, c_owner_id, c_db_fullschema, c_db_datasource, c_db_servertype, c_db_nicename, c_db_tooltip, c_comment, c_entry_date, c_change_date, c_status_cd)
   VALUES('i2b2demo', 'Demo/', '@', 'i2b2metadata', 'java:/OntologyDemoDS', 'ORACLE', 'Metadata', NULL, NULL, NULL, NULL, NULL);
+INSERT INTO ONT_DB_LOOKUP(c_domain_id, c_project_path, c_owner_id, c_db_fullschema, c_db_datasource, c_db_servertype, c_db_nicename, c_db_tooltip, c_comment, c_entry_date, c_change_date, c_status_cd)
+  VALUES('i2b2demo', 'ACT/', '@', 'i2b2actata', 'java:/OntologyACTDS', 'ORACLE', 'Metadata', NULL, NULL, NULL, NULL, NULL);
+  
