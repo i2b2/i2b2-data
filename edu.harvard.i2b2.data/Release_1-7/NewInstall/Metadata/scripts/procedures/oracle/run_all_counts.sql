df14d14d22134214f4cf02c65b1d04fc07a0c0a3
mem61 <mem61@192.168.6.127>
2019-06-13 14:27:07 -0400

    Added stored procedure call for total num

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/scripts/procedures/oracle/run_all_counts.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/scripts/procedures/oracle/run_all_counts.sql
@@ -1,5 +1,4 @@
-create or replace PROCEDURE              runtotalnum  (observationTable IN VARCHAR, 
-   errorMsg OUT VARCHAR)
+create or replace PROCEDURE                           runtotalnum  (observationTable IN VARCHAR, schemaName in VARCHAR)
 IS
 
 
@@ -7,13 +6,15 @@ IS
  curRecord   distinctTableCurTyp;
  sql_stmt  varchar2(2000);
  dis_c_table_name varchar2(700);
-
+errorMsg VARCHAR2(700);
 
 BEGIN
 
  sql_stmt := 'select distinct c_table_name from TABLE_ACCESS where c_visualattributes like ''%A%''  ';
  
 
+    -- rather than creating cursor and fetching rows into local variables, instead using record variable type to 
+    -- access each element of the current row of the cursor
  open curRecord for sql_stmt ;
  
    loop
@@ -21,11 +22,12 @@ BEGIN
       EXIT WHEN curRecord%NOTFOUND;
 
  EXECUTE IMMEDIATE 'update ' || dis_c_table_name || ' set c_totalnum=null';
- PAT_COUNT_VISITS( dis_c_table_name , observationTable ,errorMsg  );
- PAT_COUNT_DIMENSIONS( dis_c_table_name , observationTable ,  'concept_cd', 'concept_dimension', 'concept_path', errorMsg  );
- PAT_COUNT_DIMENSIONS( dis_c_table_name , observationTable ,  'provider_id', 'provider_dimension', 'provider_path', errorMsg  );
- PAT_COUNT_DIMENSIONS( dis_c_table_name , observationTable ,  'modifier_cd', 'modifier_dimension', 'modifier_path', errorMsg  );
+ PAT_COUNT_VISITS( dis_c_table_name , schemaName ,errorMsg  );
+ PAT_COUNT_DIMENSIONS( dis_c_table_name , schemaName, observationTable ,  'concept_cd', 'concept_dimension', 'concept_path', errorMsg  );
+ PAT_COUNT_DIMENSIONS( dis_c_table_name , schemaName,  observationTable ,  'provider_id', 'provider_dimension', 'provider_path', errorMsg  );
+ PAT_COUNT_DIMENSIONS( dis_c_table_name , schemaName, observationTable ,  'modifier_cd', 'modifier_dimension', 'modifier_path', errorMsg  );
 
  END LOOP;
 
+ -- :ERRORMSG := ERRORMSG;
 END;
