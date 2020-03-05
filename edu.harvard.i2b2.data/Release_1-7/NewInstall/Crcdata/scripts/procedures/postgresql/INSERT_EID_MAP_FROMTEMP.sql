f31852f71b038deed502b774a20db644757ef4be
Michael Mendis <mmendis@partners.org>
2020-03-05 15:25:52 -0500

    Update INSERT_EID_MAP_FROMTEMP.sql

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata/scripts/procedures/postgresql/INSERT_EID_MAP_FROMTEMP.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata/scripts/procedures/postgresql/INSERT_EID_MAP_FROMTEMP.sql
@@ -8,8 +8,8 @@ maxEncounterNum bigint;
 distinctEidCur REFCURSOR;
 disEncounterId varchar(100); 
 disEncounterIdSource varchar(100);
-disPatiendId varchar(100);
-disPatiendIdSource varchar(100);
+disPatientId varchar(100);
+disPatientIdSource varchar(100);
 
 BEGIN
     EXECUTE ' delete  from ' || tempEidTableName ||  ' t1  where 
@@ -23,14 +23,14 @@ BEGIN
     if coalesce(maxEncounterNum::text, '') = '' then 
         maxEncounterNum := 0;
     end if;
-    open distinctEidCur for EXECUTE 'SELECT distinct encounter_id,encounter_id_source,patient_map_id,patient_map_id_source  from ' || tempEidTableName ||' ' ;
+    open distinctEidCur for EXECUTE 'SELECT distinct encounter_id,encounter_id_source,patient_map_id,patient_map_id_source from ' || tempEidTableName ||' ' ;
     loop
-        FETCH distinctEidCur INTO disEncounterId, disEncounterIdSource, disPatiendId, disPatiendIdSource;
-        IF NOT FOUND THEN EXIT; END IF; -- apply on distinctEidCur
-            -- dbms_output.put_line(disEncounterId);
+        FETCH distinctEidCur INTO disEncounterId, disEncounterIdSource,disPatientId,disPatientIdSource;
+        IF NOT FOUND THEN EXIT; END IF; 
+            
             if  disEncounterIdSource = 'HIVE'  THEN 
                 begin
-                    --check if hive number exist, if so assign that number to reset of map_id's within that pid
+                    
                     select encounter_num into existingEncounterNum from encounter_mapping where encounter_num = CAST(disEncounterId AS numeric) and encounter_ide_source = 'HIVE';
                     EXCEPTION  when NO_DATA_FOUND THEN
                         existingEncounterNum := null;
@@ -40,7 +40,7 @@ BEGIN
                     where encounter_id = $1 and not exists (select 1 from encounter_mapping em where em.encounter_ide = encounter_map_id
                         and em.encounter_ide_source = encounter_map_id_source)' using disEncounterId;
                 else 
-                    -- generate new patient_num i.e. take max(_num) + 1 
+                    
                     if maxEncounterNum < CAST(disEncounterId AS numeric) then 
                         maxEncounterNum := disEncounterId;
                     end if ;
@@ -48,13 +48,13 @@ BEGIN
                     encounter_id =  $1 and encounter_id_source = ''HIVE'' and not exists (select 1 from encounter_mapping em where em.encounter_ide = encounter_map_id
                         and em.encounter_ide_source = encounter_map_id_source)' using disEncounterId;
                 end if;    
-                -- test if record fectched
-                -- dbms_output.put_line(' HIVE ');
+                
+                
             else 
                 begin
                     select encounter_num into STRICT  existingEncounterNum from encounter_mapping where encounter_ide = disEncounterId and 
                     encounter_ide_source = disEncounterIdSource and patient_ide=disPatientId and patient_ide_source=disPatientIdSource; 
-                    -- test if record fetched. 
+                    
                     EXCEPTION
     WHEN NO_DATA_FOUND THEN
         existingEncounterNum := null;
@@ -62,24 +62,24 @@ BEGIN
                 if existingEncounterNum is not  null then 
                     EXECUTE ' update ' || tempEidTableName ||' set encounter_num = CAST($1 AS numeric) , process_status_flag = ''P''
                     where encounter_id = $2 and not exists (select 1 from encounter_mapping em where em.encounter_ide = encounter_map_id
-                        and em.encounter_ide_source = encounter_map_id_source)' using existingEncounterNum, disEncounterId;
+                        and em.encounter_ide_source = encounter_map_id_source and em.patient_ide_source = patient_map_id_source and em.patient_ide=patient_map_id)' using existingEncounterNum, disEncounterId;
                 else 
                     maxEncounterNum := maxEncounterNum + 1 ;
-                    --TODO : add update colunn
+                    
                     EXECUTE ' insert into ' || tempEidTableName ||' (encounter_map_id,encounter_map_id_source,encounter_id,encounter_id_source,encounter_num,process_status_flag
                         ,encounter_map_id_status,update_date,download_date,import_date,sourcesystem_cd,patient_map_id,patient_map_id_source) 
-                    values($1,''HIVE'',$2,''HIVE'',$3,''P'',''A'',Now(),Now(),Now(),''edu.harvard.i2b2.crc'')' using maxEncounterNum,maxEncounterNum,maxEncounterNum,disPatiendId,disPatiendIdSource;
+                    values($1,''HIVE'',$2,''HIVE'',$3,''P'',''A'',Now(),Now(),Now(),''edu.harvard.i2b2.crc'',$4,$5)' using maxEncounterNum,maxEncounterNum,maxEncounterNum,disPatientId,disPatientIdSource; 
                     EXECUTE ' update ' || tempEidTableName ||' set encounter_num =  $1 , process_status_flag = ''P'' 
                     where encounter_id = $2 and  not exists (select 1 from 
                         encounter_mapping em where em.encounter_ide = encounter_map_id
                         and em.encounter_ide_source = encounter_map_id_source
                         and em.patient_ide_source = patient_map_id_source and em.patient_ide=patient_map_id)' using maxEncounterNum, disEncounterId;
                 end if ;
-                -- dbms_output.put_line(' NOT HIVE ');
+                
             end if; 
     END LOOP;
     close distinctEidCur ;
-    -- do the mapping update if the update date is old
+    
 
 EXECUTE 'UPDATE encounter_mapping
 SET 
@@ -96,14 +96,14 @@ FROM '|| tempEidTableName || '  temp
 WHERE 
 temp.encounter_map_id = encounter_mapping.encounter_ide 
 and temp.encounter_map_id_source = encounter_mapping.encounter_ide_source
-and temp.patient_map_id  encounter_mapping.patient_ide 
-and temp.patient_ide_source  =	encounter_mapping.patient_map_id_source
+and temp.patient_map_id = encounter_mapping.patient_ide 
+and temp.patient_map_id_source = encounter_mapping.patient_ide_source
 and temp.encounter_id_source = ''HIVE''
 and coalesce(temp.process_status_flag::text, '''') = ''''  
 and coalesce(encounter_mapping.update_date,to_date(''01-JAN-1900'',''DD-MON-YYYY'')) <= coalesce(temp.update_date,to_date(''01-JAN-1900'',''DD-MON-YYYY''))
 ';
 
-    -- insert new mapping records i.e flagged P
+    
     EXECUTE ' insert into encounter_mapping (encounter_ide,encounter_ide_source,encounter_ide_status,encounter_num,patient_ide,patient_ide_source,update_date,download_date,import_date,sourcesystem_cd,upload_id,project_id) 
     SELECT encounter_map_id,encounter_map_id_source,encounter_map_id_status,encounter_num,patient_map_id,patient_map_id_source,update_date,download_date,Now(),sourcesystem_cd,' || upload_id || ' , ''@'' project_id
     FROM ' || tempEidTableName || '  
@@ -112,5 +112,5 @@ and coalesce(encounter_mapping.update_date,to_date(''01-JAN-1900'',''DD-MON-YYYY
     WHEN OTHERS THEN
         RAISE EXCEPTION 'An error was encountered - % -ERROR- %',SQLSTATE,SQLERRM;
     end;
-    $body$
-    LANGUAGE PLPGSQL;
+     $body$
+LANGUAGE 'plpgsql'
