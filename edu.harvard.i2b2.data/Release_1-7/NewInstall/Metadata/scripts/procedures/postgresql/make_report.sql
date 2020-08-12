dd2279c851a44d2608479118102adc2f184f7dda
Jeff Klann <jklann@gmail.com>
2020-08-12 17:39:44 -0400

    CORE-394 Fixed potential bug in reporting table for Postgres

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/scripts/procedures/postgresql/make_report.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/scripts/procedures/postgresql/make_report.sql
@@ -29,7 +29,8 @@ BEGIN
     insert into totalnum_report(c_fullname, agg_count, agg_date)
      select c_fullname, case sign(agg_count - threshold + 1 ) when 1 then (round(agg_count/5.0,0)*5)+round(random_normal(1,0,sigma,threshold)) else -1 end agg_count, 
        to_char(agg_date,'YYYY-MM-DD') agg_date from 
-       (select row_number() over (partition by c_fullname order by agg_date desc) rn,c_fullname, agg_count,agg_date from totalnum where typeflag_cd like 'P%') x where rn=1;
+       (select * from 
+           (select row_number() over (partition by c_fullname order by agg_date desc) rn,c_fullname, agg_count,agg_date from totalnum where typeflag_cd like 'P%') x where rn=1) y;
 
     update totalnum_report set agg_count=-1 where agg_count<threshold;
 
