59630f15c2f62867b4704337a512d24dce5a6b6d
Jeff Klann <jklann@gmail.com>
2020-08-18 13:58:36 -0400

    CORE-394 Fixed Postgres 10 bug with totalnums

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/scripts/procedures/postgresql/make_report.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/scripts/procedures/postgresql/make_report.sql
@@ -27,7 +27,7 @@ BEGIN
     truncate table totalnum_report;
 
     insert into totalnum_report(c_fullname, agg_count, agg_date)
-     select c_fullname, case sign(agg_count - threshold + 1 ) when 1 then (round(agg_count/5.0,0)*5)+round(random_normal(1,0,sigma,threshold)) else -1 end agg_count, 
+     select c_fullname, case sign(agg_count - threshold + 1 ) when 1 then (round(agg_count/5.0,0)*5)+round(random_normal(0,sigma,threshold)) else -1 end agg_count, 
        to_char(agg_date,'YYYY-MM-DD') agg_date from 
        (select * from 
            (select row_number() over (partition by c_fullname order by agg_date desc) rn,c_fullname, agg_count,agg_date from totalnum where typeflag_cd like 'P%') x where rn=1) y;
