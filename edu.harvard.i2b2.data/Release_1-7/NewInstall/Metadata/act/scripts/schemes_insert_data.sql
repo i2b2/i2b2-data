01488ff1367717fda2799809bc531384382d6a94
mem61 <mem61@192.168.6.145>
2021-07-13 15:53:10 -0400

    DATA-9 Load ACT version 4 Ontology

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/act/scripts/schemes_insert_data.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/act/scripts/schemes_insert_data.sql
@@ -1,23 +1,31 @@
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('NDC:','NDC','National Drug Code');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DSG-NLP:','DSG-NLP','Natural Language Processing Data');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('UMLS:','UMLS','United Medical Language System');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD9CM:','ICD9CM','ICD9 code for diagnoses and procedures');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD10CM:','ICD10CM','ICD10 code for diagnoses');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('LOINC:','LOINC','Lab codes');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|AGE:','DEM|AGE','Age of patient from demographics data');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|DATE:','DEM|DATE','Patient date of birth from demographics data');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|MARITAL:','DEM|MARITAL','Marital Status of patient');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|LANGUAGE:','DEM|LANGUAGE','Primary language spoken by patient');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|RACE:','DEM|RACE','Race of patient');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|RELIGION:','DEM|RELIGION','Religion of patient');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|SEX:','DEM|SEX','Gender of patient');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|VITAL STATUS:','DEM|VITAL STATUS','Vital status of patient');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|ZIPCODE:','DEM|ZIPCODE',null);
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('(null)','None','No scheme');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('RXNORM:','RXNORM','RxNorm');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|HISP:','DEM|HISP','Hispanic origins');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('NUI:','NDF-RT','NDF-RT VA Drug Classification');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD9PROC:','ICD9PROC','ICD9 code for procedures');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD10PCS:','ICD10PCS','ICD10 code for procedures');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('CPT4:','CPT4','Current Procedural Terminology');
-INSERT into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('HCPCS:','HCPCS','Healthcare Common Procedure Coding System');
+--------------------------------------------------------
+--  File created - Monday-May-24-2021   
+--------------------------------------------------------
+REM INSERTING into SCHEMES
+SET DEFINE OFF;
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('NDC:','NDC','National Drug Code');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DSG-NLP:','DSG-NLP','Natural Language Processing Data');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('UMLS:','UMLS','United Medical Language System');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD9CM:','ICD9CM','ICD9 code for diagnoses and procedures');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD10CM:','ICD10CM','ICD10 code for diagnoses');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('LOINC:','LOINC','Lab codes');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|AGE:','DEM|AGE','Age of patient from demographics data');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|DATE:','DEM|DATE','Patient date of birth from demographics data');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|MARITAL:','DEM|MARITAL','Marital Status of patient');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|LANGUAGE:','DEM|LANGUAGE','Primary language spoken by patient');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|RACE:','DEM|RACE','Race of patient');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|RELIGION:','DEM|RELIGION','Religion of patient');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|SEX:','DEM|SEX','Gender of patient');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|VITAL STATUS:','DEM|VITAL STATUS','Vital status of patient');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|ZIPCODE:','DEM|ZIPCODE',null);
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('(null)','None','No scheme');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('RXNORM:','RXNORM','RxNorm');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('DEM|HISP:','DEM|HISP','Hispanic origins');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('NUI:','NDF-RT','NDF-RT VA Drug Classification');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD9PROC:','ICD9PROC','ICD9 code for procedures');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ICD10PCS:','ICD10PCS','ICD10 code for procedures');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('CPT4:','CPT4','Current Procedural Terminology');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('HCPCS:','HCPCS','Healthcare Common Procedure Coding System');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('ATC:','ATC','Anatomical Therapeutic Chemical ');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('SNOMED:','SNOMED','SNOMED CT');
+Insert into SCHEMES (C_KEY,C_NAME,C_DESCRIPTION) values ('CVX:','CVX','Vaccine Administered');
