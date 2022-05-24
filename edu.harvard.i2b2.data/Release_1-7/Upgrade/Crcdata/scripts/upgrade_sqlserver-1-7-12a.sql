1f4b60897d6e76c0153568f2d18cc6d96de2356a
Jeff Klann <jklann@gmail.com>
2022-05-24 14:53:46 -0400

    Update for 1.7.13

--- edu.harvard.i2b2.data/Release_1-7/Upgrade/Crcdata/scripts/upgrade_sqlserver-1-7-12a.sql
+++ edu.harvard.i2b2.data/Release_1-7/Upgrade/Crcdata/scripts/upgrade_sqlserver-1-7-12a.sql
@@ -2,10 +2,11 @@
 --==============================================================
 -- Database Script to upgrade CRC from 1.7.12a to 1.7.13                
 --==============================================================
--- if the following command belows fails due to lack of temp space, rename PATIENT_ENC_COLL_ID to QT_PATIENT_ENC_COLLECTION_BAK, create a new table QT_PATIENT_ENC_COLLECTION 
+-- This line must be executed as follows due to the primary key:
+-- rename PATIENT_ENC_COLL_ID to QT_PATIENT_ENC_COLLECTION_BAK, create a new table QT_PATIENT_ENC_COLLECTION 
 -- and copy data from QT_PATIENT_ENC_COLLECTION_BAK to QT_PATIENT_ENC_COLLECTION
-alter table QT_PATIENT_ENC_COLLECTION alter column  PATIENT_ENC_COLL_ID BIGINT NOT NULL
-;
+--alter table QT_PATIENT_ENC_COLLECTION alter column  PATIENT_ENC_COLL_ID BIGINT NOT NULL
+--;
                                                                  
 -- Set age_in_num_years values in PATIENT_DIMENSION 
 UPDATE PATIENT_DIMENSION
