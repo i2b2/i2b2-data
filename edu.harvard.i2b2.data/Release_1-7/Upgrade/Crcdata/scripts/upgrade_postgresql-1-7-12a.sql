214a77affe8b342d175d49026fe739f7ed31094d
Jeff Klann <jklann@gmail.com>
2022-05-24 14:49:32 -0400

    Update script for 1.7.13

--- edu.harvard.i2b2.data/Release_1-7/Upgrade/Crcdata/scripts/upgrade_postgresql-1-7-12a.sql
+++ edu.harvard.i2b2.data/Release_1-7/Upgrade/Crcdata/scripts/upgrade_postgresql-1-7-12a.sql
@@ -1,8 +1,10 @@
 --==============================================================
 -- Database Script to upgrade CRC from 1.7.12a to 1.7.13                 
 --==============================================================
-alter table QT_PATIENT_SET_COLLECTION alter column PATIENT_SET_COLL_ID BIGSERIAL
-;
+-- Optional: support larger patient encounter collections
+-- You must temorarily disable the primary key and run this line manually
+--alter table QT_PATIENT_SET_COLLECTION alter column PATIENT_SET_COLL_ID BIGSERIAL
+--;
 -- Set age_in_num_years values in PATIENT_DIMENSION 
 UPDATE PATIENT_DIMENSION
 Set AGE_IN_YEARS_NUM =
