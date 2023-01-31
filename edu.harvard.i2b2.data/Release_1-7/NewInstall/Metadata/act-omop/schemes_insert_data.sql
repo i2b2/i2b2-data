26c533ca5a79fcd958766b2991b58e9bc2baf91c
mem61 <mmendis@partners.org>
2023-01-31 14:37:29 -0500

    Added act omop for sqlserver

---
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Metadata/act-omop/schemes_insert_data.sql
@@ -0,0 +1,45 @@
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('(null)', 'None', 'No scheme');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('CPT4:', 'CPT4', 'Current Procedural Terminology');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|AGE:', 'DEM|AGE', 'Age of patient from demographics data');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|DATE:', 'DEM|DATE', 'Patient date of birth from demographics data');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|HISP:', 'DEM|HISP', 'Hispanic origins');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|LANGUAGE:', 'DEM|LANGUAGE', 'Primary language spoken by patient');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|MARITAL:', 'DEM|MARITAL', 'Marital Status of patient');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|RACE:', 'DEM|RACE', 'Race of patient');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|RELIGION:', 'DEM|RELIGIO', 'Religion of patient');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|SEX:', 'DEM|SEX', 'Gender of patient');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|VITAL STATUS:', 'DEM|VITAL STATUS', 'Vital status of patient');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DEM|ZIPCODE:', 'DEM|ZIPCODE', NULL);
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('DSG-NLP:', 'DSG-NLP', 'Natural Language Processing Data');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('HCPCS:', 'HCPCS', 'Healthcare Common Procedure Coding System');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('ICD10CM:', 'ICD10CM', 'ICD10 code for diagnoses');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('ICD10PCS:', 'ICD10PCS', 'ICD10 code for procedures');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('ICD9CM:', 'ICD9CM', 'ICD9 code for diagnoses and procedures');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('ICD9PROC:', 'ICD9PROC', 'ICD9 code for procedures');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('LOINC:', 'LOINC', 'Lab codes');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('NDC:', 'NDC', 'National Drug Code');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('NUI:', 'NDF-RT', 'NDF-RT VA Drug Classificatio');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('RXNORM:', 'RXNORM', 'RxNorm');
+
+INSERT schemes (C_KEY, C_NAME, C_DESCRIPTION) VALUES ('UMLS:', 'UMLS', 'United Medical Language System');
