cc87e21142ecc387c74c933027929fb63ca0eae5
Jeff Klann <jklann@gmail.com>
2023-03-01 18:05:35 -0500

    Aligned columns across views so PDO will work. Added individual source views.

--- edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata/scripts/act-omop/sqlserver/BUILD_ACT_OMOP_VIEWS_ALL_MSSQL.sql
+++ edu.harvard.i2b2.data/Release_1-7/NewInstall/Crcdata/scripts/act-omop/sqlserver/BUILD_ACT_OMOP_VIEWS_ALL_MSSQL.sql
@@ -19,6 +19,21 @@ DROP VIEW VISIT_DIMENSION;
 If OBJECT_ID('DEVICE_VIEW','V') is not null
 DROP VIEW DEVICE_VIEW;
 
+If OBJECT_ID('DX_SOURCE_CONCEPTS','V') is not null
+DROP VIEW  DX_SOURCE_CONCEPTS;
+If OBJECT_ID('DRUG_SOURCE_CONCEPTS','V') is not null
+DROP VIEW DRUG_SOURCE_CONCEPTS;
+If OBJECT_ID('MEASUREMENT_SOURCE_CONCEPTS','V') is not null
+DROP VIEW MEASUREMENT_SOURCE_CONCEPTS;
+If OBJECT_ID('OBSERVATION_SOURCE_CONCEPTS','V') is not null
+DROP VIEW OBSERVATION_SOURCE_CONCEPTS;
+If OBJECT_ID('PROCEDURE_SOURCE_CONCEPTS','V') is not null
+DROP VIEW PROCEDURE_SOURCE_CONCEPTS;
+If OBJECT_ID('COVID_LAB_SOURCE_CONCEPTS','V') is not null
+DROP VIEW COVID_LAB_SOURCE_CONCEPTS;
+If OBJECT_ID('DEVICE_SOURCE_CONCEPTS','V') is not null
+DROP VIEW DEVICE_SOURCE_CONCEPTS;
+
 /****** Object:  View ALL_SOURCE_CONCEPTS  ******/
 
 CREATE  VIEW ALL_SOURCE_CONCEPTS (ENCOUNTER_NUM, PATIENT_NUM, CONCEPT_CD, PROVIDER_ID, START_DATE, MODIFIER_CD, 
@@ -140,6 +155,8 @@ DATEDIFF(hour, birth_datetime,
 FROM            PERSON
 ;
 
+/******* OMOP FACT TABLE VIEWS ************/
+
 CREATE  VIEW CONDITION_VIEW AS 
 SELECT  
 			visit_occurrence_id AS ENCOUNTER_NUM, 
@@ -162,6 +179,7 @@ SELECT
             NULL DOWNLOAD_DATE, 
             NULL IMPORT_DATE, 
             NULL UPLOAD_ID,
+            NULL as quantity_num,
    			condition_SOURCE_concept_id AS SOURCE_CONCEPT_ID,
             condition_source_value AS SOURCE_VALUE,
             'CONDITION' AS DOMAIN_ID
@@ -196,6 +214,7 @@ SELECT
             NULL DOWNLOAD_DATE, 
             NULL IMPORT_DATE, 
             NULL UPLOAD_ID,
+            NULL as quantity_num,
    			DEVICE_SOURCE_CONCEPT_ID AS SOURCE_CONCEPT_ID, --device_concept_id??
             device_source_value AS SOURCE_VALUE,
             'DEVICE' AS DOMAIN_ID
@@ -228,6 +247,7 @@ SELECT
             NULL DOWNLOAD_DATE, 
             NULL IMPORT_DATE, 
             NULL UPLOAD_ID,
+            NULL as quantity_num,
    			drug_source_concept_id AS SOURCE_CONCEPT_ID,
             drug_source_value AS SOURCE_VALUE,
             'DRUG' AS DOMAIN_ID
@@ -249,20 +269,20 @@ FROM DRUG_EXPOSURE;
 
  CREATE  VIEW MEASUREMENT_VIEW as
   SELECT
+	VISIT_OCCURRENCE_ID ENCOUNTER_NUM,
 	PERSON_ID PATIENT_NUM,
 	CAST(MEASUREMENT_CONCEPT_ID AS VARCHAR(50)) CONCEPT_CD,
-	VISIT_OCCURRENCE_ID ENCOUNTER_NUM,
 	CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
 	MEASUREMENT_DATE AS START_DATE,
     NULL AS end_DATE,
 	'@' AS MODIFIER_CD,
 	1 INSTANCE_NUM,
-    NULL AS LOCATION_CD,
 	CASE 
 		WHEN VALUE_AS_NUMBER IS NOT NULL 
 		THEN 'N' 
 		ELSE 'T' 
 	END AS VALTYPE_CD,
+    NULL AS LOCATION_CD,
 	CASE 
 		WHEN OPERATOR_CONCEPT_ID = 4172703 
 		THEN 'E' 
@@ -287,6 +307,7 @@ FROM DRUG_EXPOSURE;
     NULL DOWNLOAD_DATE, 
     NULL IMPORT_DATE, 
     NULL UPLOAD_ID,
+    NULL as quantity_num,
 	measurement_source_concept_id AS SOURCE_CONCEPT_ID, 
     measurement_source_value AS SOURCE_VALUE,
     'MEASUREMENT' AS DOMAIN_ID
@@ -295,20 +316,20 @@ FROM
 
 CREATE  VIEW OBSERVATION_VIEW AS
 SELECT
+	VISIT_OCCURRENCE_ID AS ENCOUNTER_NUM,
 	PERSON_ID AS PATIENT_NUM,
     CAST(OBSERVATION_CONCEPT_ID AS VARCHAR(50)) AS CONCEPT_CD, 
-	VISIT_OCCURRENCE_ID AS ENCOUNTER_NUM,
-	1 as INSTANCE_NUM,
     CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
 	OBSERVATION_DATE AS START_DATE,
     NULL AS END_DATE,
 	'@' AS MODIFIER_CD,
-	NULL AS LOCATION_CD,
+	1 as INSTANCE_NUM,
     CASE 
        WHEN VALUE_AS_NUMBER IS NOT NULL 
        THEN 'N' 
        ELSE 'T' 
     END AS valtype_cd, --DECODE THIS IS THE FUTURE operator_concept_id
+	NULL AS LOCATION_CD,
     CASE 
         WHEN VALUE_AS_NUMBER IS NOT NULL THEN 'E' 
         ELSE VALUE_AS_STRING 
@@ -363,6 +384,7 @@ SELECT
 FROM PROCEDURE_OCCURRENCE;
 
 /****** Object:  View COVID_LAB_VIEW ******/
+/* Columns are not in the right order */
 CREATE  VIEW COVID_LAB_VIEW 
 (PATIENT_NUM, CONCEPT_CD, ENCOUNTER_NUM, INSTANCE_NUM, PROVIDER_ID, START_DATE, MODIFIER_CD, 
 OBSERVATION_BLOB, VALTYPE_CD, TVAL_CHAR, 
@@ -412,3 +434,230 @@ WHERE MEASUREMENT_SOURCE_CONCEPT_ID IN (586515,586516,586517,586518,586519,58652
 706173,706174,706175,706177,706178,706180,706181,715260,715261,715262,715272,723459,723463,723464,723465,723466,723467,723468,
 723469,723470,723471,723472,723473,723474,723475,723476,723477,723478,723479,723480,757677,757678,757679,757680,757685,757686,
 36659631,36661369,36661370,36661371,36661372,36661373,36661374,36661375,36661377,36661378) ;
+
+/************************ OMOP_SOURCE_FACT TABLE VIEWS ***************************/
+CREATE  VIEW DX_SOURCE_CONCEPTS AS 
+SELECT  
+			visit_occurrence_id AS ENCOUNTER_NUM, 
+ 			PERSON_ID AS PATIENT_NUM, 
+   			cast(condition_source_concept_id as varchar(50)) AS CONCEPT_CD, 
+   			CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+   			condition_start_datetime AS START_DATE, 
+			condition_end_datetime AS END_DATE, 
+            NULL AS MODIFIER_CD,
+            NULL AS INSTANCE_NUM,
+            NULL AS valtype_cd,
+            NULL AS location_cd,
+            NULL AS tval_char,
+            NULL AS nval_num,
+            NULL AS valueflag_cd,
+            NULL AS units_cd,
+            NULL CONFIDENCE_NUM, 
+            NULL SOURCESYSTEM_CD, 
+            NULL UPDATE_DATE, 
+            NULL DOWNLOAD_DATE, 
+            NULL IMPORT_DATE, 
+            NULL UPLOAD_ID,
+            NULL as quantity_num,
+   			condition_SOURCE_concept_id AS SOURCE_CONCEPT_ID,
+            condition_source_value AS SOURCE_VALUE,
+            'CONDITION' AS DOMAIN_ID
+--			condition_occurrence_id integer NOT NULL, 
+--			condition_start_datetime TIMESTAMP NULL, 
+--			condition_end_datetime TIMESTAMP NULL, 
+--			condition_type_concept_id integer NOT NULL, --modifier?
+--			stop_reason varchar(20) NULL, 
+--			visit_detail_id integer NULL, 
+--			condition_status_source_value varchar(50) NULL );  
+FROM CONDITION_OCCURRENCE;
+
+CREATE  VIEW DEVICE_SOURCE_CONCEPTS AS 
+SELECT 
+			visit_occurrence_id AS ENCOUNTER_NUM, 
+ 			PERSON_ID AS PATIENT_NUM,
+   			CAST(device_source_concept_id AS VARCHAR(50)) AS CONCEPT_CD, 
+   			CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+   			device_exposure_start_datetime AS START_DATE, 
+			device_exposure_end_datetime AS END_DATE, 
+            device_type_concept_id AS MODIFIER_CD,
+            NULL AS INSTANCE_NUM,
+            NULL AS valtype_cd,
+            NULL AS location_cd,
+            NULL AS tval_char,
+            NULL AS nval_num,
+            NULL AS valueflag_cd,
+            NULL AS units_cd,
+            NULL CONFIDENCE_NUM, 
+            NULL SOURCESYSTEM_CD, 
+            NULL UPDATE_DATE, 
+            NULL DOWNLOAD_DATE, 
+            NULL IMPORT_DATE, 
+            NULL UPLOAD_ID,
+            NULL as quantity_num,
+   			DEVICE_SOURCE_CONCEPT_ID AS SOURCE_CONCEPT_ID, --device_concept_id??
+            device_source_value AS SOURCE_VALUE,
+            'DEVICE' AS DOMAIN_ID
+--			device_exposure_start_datetime TIMESTAMP NULL, 
+--			device_exposure_end_datetime TIMESTAMP NULL, 
+--			unique_device_id varchar(50) NULL, 
+--			quantity integer NULL, 
+--			visit_detail_id integer NULL, 
+FROM DEVICE_EXPOSURE;
+
+CREATE  VIEW DRUG_SOURCE_CONCEPTS AS 
+SELECT 
+ 			visit_occurrence_id AS ENCOUNTER_NUM, 
+ 			PERSON_ID AS PATIENT_NUM, 
+   			cast(drug_source_concept_id as varchar(50)) AS CONCEPT_CD, 
+   			CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+   			drug_exposure_start_datetime AS START_DATE, 
+			drug_exposure_END_datetime AS END_DATE, 
+            NULL AS MODIFIER_CD,
+            NULL AS INSTANCE_NUM,
+            NULL AS valtype_cd,
+            NULL AS location_cd,
+            NULL AS tval_char,
+            NULL AS nval_num,
+            NULL AS valueflag_cd,
+            NULL AS units_cd,
+            NULL CONFIDENCE_NUM, 
+            NULL SOURCESYSTEM_CD, 
+            NULL UPDATE_DATE, 
+            NULL DOWNLOAD_DATE, 
+            NULL IMPORT_DATE, 
+            NULL UPLOAD_ID,
+            NULL as quantity_num,
+   			drug_source_concept_id AS SOURCE_CONCEPT_ID,
+            drug_source_value AS SOURCE_VALUE,
+            'DRUG' AS DOMAIN_ID
+--			drug_exposure_start_datetime TIMESTAMP NULL, 
+--			drug_exposure_end_datetime TIMESTAMP NULL, 
+--			verbatim_end_date date NULL, 
+--			drug_type_concept_id integer NOT NULL, 
+--			stop_reason varchar(20) NULL, 
+--			refills integer NULL, 
+--			quantity float NULL, 
+--			days_supply integer NULL, 
+--			sig CLOB NULL, 
+--			route_concept_id integer NULL, 
+--			lot_number varchar(50) NULL, 
+--			visit_detail_id integer NULL, 
+--			route_source_value varchar(50) NULL, 
+--			dose_unit_source_value varchar(50) NULL );  
+FROM DRUG_EXPOSURE;
+
+ CREATE  VIEW MEASUREMENT_SOURCE_CONCEPTS as
+  SELECT
+	VISIT_OCCURRENCE_ID ENCOUNTER_NUM,
+	PERSON_ID PATIENT_NUM,
+	CAST(MEASUREMENT_SOURCE_CONCEPT_ID AS VARCHAR(50)) CONCEPT_CD,
+	CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+	MEASUREMENT_DATE AS START_DATE,
+    NULL AS end_DATE,
+	'@' AS MODIFIER_CD,
+	1 INSTANCE_NUM,
+	CASE 
+		WHEN VALUE_AS_NUMBER IS NOT NULL 
+		THEN 'N' 
+		ELSE 'T' 
+	END AS VALTYPE_CD,
+    NULL AS LOCATION_CD,
+	CASE 
+		WHEN OPERATOR_CONCEPT_ID = 4172703 
+		THEN 'E' 
+		WHEN OPERATOR_CONCEPT_ID = 4171756 
+		THEN 'LT' 
+		WHEN OPERATOR_CONCEPT_ID = 4172704 
+		THEN 'GT' 
+		WHEN OPERATOR_CONCEPT_ID = 4171754 
+		THEN 'LE' 
+		WHEN OPERATOR_CONCEPT_ID = 4171755 
+		THEN 'GE' 
+		WHEN OPERATOR_CONCEPT_ID IS NULL 
+		THEN 'E' 
+		ELSE NULL 
+	END AS TVAL_CHAR ,
+	VALUE_AS_NUMBER AS NVAL_NUM,
+   	CAST(VALUE_AS_CONCEPT_ID AS VARCHAR(50)) AS VALUEFLAG_CD,
+    UNIT_SOURCE_VALUE AS UNITS_CD, 
+    NULL CONFIDENCE_NUM, 
+    NULL SOURCESYSTEM_CD, 
+    NULL UPDATE_DATE, 
+    NULL DOWNLOAD_DATE, 
+    NULL IMPORT_DATE, 
+    NULL UPLOAD_ID,
+    NULL as quantity_num,
+	measurement_source_concept_id AS SOURCE_CONCEPT_ID, 
+    measurement_source_value AS SOURCE_VALUE,
+    'MEASUREMENT' AS DOMAIN_ID
+FROM
+	MEASUREMENT;
+
+CREATE  VIEW OBSERVATION_SOURCE_CONCEPTS AS
+SELECT
+	VISIT_OCCURRENCE_ID AS ENCOUNTER_NUM,
+	PERSON_ID AS PATIENT_NUM,
+    CAST(OBSERVATION_SOURCE_CONCEPT_ID AS VARCHAR(50)) AS CONCEPT_CD, 
+    CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+	OBSERVATION_DATE AS START_DATE,
+    NULL AS END_DATE,
+	'@' AS MODIFIER_CD,
+	1 as INSTANCE_NUM,
+    CASE 
+       WHEN VALUE_AS_NUMBER IS NOT NULL 
+       THEN 'N' 
+       ELSE 'T' 
+    END AS valtype_cd, --DECODE THIS IS THE FUTURE operator_concept_id
+	NULL AS LOCATION_CD,
+    CASE 
+        WHEN VALUE_AS_NUMBER IS NOT NULL THEN 'E' 
+        ELSE OBSERVATION_SOURCE_VALUE 
+        END AS TVAL_CHAR,
+    VALUE_AS_NUMBER AS NVAL_NUM,
+    CAST(VALUE_AS_CONCEPT_ID AS VARCHAR(50)) AS VALUEFLAG_CD,
+    UNIT_SOURCE_VALUE AS UNITS_CD,
+    NULL CONFIDENCE_NUM, 
+            NULL SOURCESYSTEM_CD, 
+            NULL UPDATE_DATE, 
+            NULL DOWNLOAD_DATE, 
+            NULL IMPORT_DATE, 
+            NULL UPLOAD_ID,
+    observation_SOURCE_concept_id AS SOURCE_CONCEPT_ID, 
+    observation_source_value AS SOURCE_VALUE,
+    'OBSERVATION' AS DOMAIN_ID
+FROM
+	OBSERVATION;
+
+CREATE  VIEW PROCEDURE_SOURCE_CONCEPTS AS 
+SELECT
+ 			visit_occurrence_id AS ENCOUNTER_NUM, 
+ 			PERSON_ID AS PATIENT_NUM, 
+   			cast(procedure_source_concept_id as varchar(50)) AS CONCEPT_CD, 
+   			CAST(provider_id AS VARCHAR(50)) AS PROVIDER_ID, 
+   			procedure_datetime AS START_DATE, 
+			NULL AS END_DATE, 
+            NULL AS MODIFIER_CD,
+            NULL AS INSTANCE_NUM,
+            NULL AS valtype_cd,
+            NULL AS location_cd,
+            NULL AS tval_char,
+            NULL AS nval_num,
+            NULL AS valueflag_cd,
+            NULL AS units_cd,
+            NULL CONFIDENCE_NUM, 
+            NULL SOURCESYSTEM_CD, 
+            NULL UPDATE_DATE, 
+            NULL DOWNLOAD_DATE, 
+            NULL IMPORT_DATE, 
+            NULL UPLOAD_ID,
+            procedure_source_concept_id AS SOURCE_CONCEPT_ID,
+            procedure_source_value AS SOURCE_VALUE,
+            'PROCEDURE' AS DOMAIN_ID
+--			procedure_occurrence_id integer NOT NULL, 
+--			procedure_datetime TIMESTAMP NULL, 
+--			procedure_type_concept_id integer NOT NULL, 
+--			modifier_concept_id integer NULL, 
+--			quantity integer NULL, 
+--			visit_detail_id integer NULL, 
+--			modifier_source_value varchar(50) NULL );  
+FROM PROCEDURE_OCCURRENCE;
