e94e4f8183762a9907af103ee506ee1893a6473a
mem61 <mem61@mgb031010.partners.org>
2024-03-12 16:05:13 -0400

    Added labs, proc and diagnosis.   also to_char to patient_num

--- edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/demo/scripts/oracle/query_export.sql
+++ edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/demo/scripts/oracle/query_export.sql
@@ -34,7 +34,7 @@ Thank you,
 The i2b2 Team </Letter>
   <Table>
 	<Filename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Demographics.csv</Filename>
-	<Query>SELECT   a.PATIENT_NUM as "i2b2 Patient Number"
+	<Query>SELECT   to_char(a.PATIENT_NUM) as "i2b2 Patient Number"
       ,a.BIRTH_DATE as "Birth Date"
       ,a.SEX_CD as "Gender"
       , a.AGE_IN_YEARS_NUM as "Age in Years"
@@ -90,7 +90,7 @@ The i2b2 Team
 </Letter>
   <Table>
 	<Filename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Medication.csv</Filename>
-	<Query>SELECT distinct   a.PATIENT_NUM as "i2b2 Patient Number"
+	<Query>SELECT distinct   to_char(a.PATIENT_NUM) as "i2b2 Patient Number"
         ,m.patient_ide as "MRN"
       ,a.start_date as "Start Date"
       ,a.end_date as "End Date"      
@@ -115,6 +115,198 @@ The i2b2 Team
    a.patient_num = v.patient_num
 left join patient_mapping m on
    a.patient_num = m.patient_num</Query>
+      	<SeparatorCharacter>\t</SeparatorCharacter>
+  </Table>
+
+</ValueExporter>',null,null,null)
+;
+insert into QT_BREAKDOWN_PATH (NAME,VALUE,CREATE_DATE,UPDATE_DATE,USER_ID) values ('PATIENT_PROCEDURE_CSV','<?xml version="1.0"?>
+<ValueExporter>
+  <Version>3.02</Version>
+  <CreationDateTime>08/09/2024 12:00:00</CreationDateTime>
+  <RequesterEmailLetter>Your request on {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_ID}}}</RequesterEmailLetter>
+  <RequestLetter>This user {{{USER_NAME}}} in project {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_ID}}}.
+  </RequestLetter>
+  <LetterFilename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Readme.txt</LetterFilename>
+  <Letter>
+Results of the i2b2 request entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, are available.
+
+Important notes about your data:
+	- Total number of patients returned in your data request: {{{PATIENT_COUNT}}}
+	- i2b2 reviewer:
+	
+Only persons specifically authorized and selected (as listed at the top of this letter) can download these files. If additional user access is needed, please ensure the person is listed on your project IRB protocol and contact the i2b2 team.
+ 
+Specifically:
+	- Remove all PHI from computer, laptop, or mobile device after analysis is completed.
+	- Do NOT share PHI or PII with anyone who is not listed on the IRB protocol.
+
+Your guideline for the storage of Protected Health Information can be found at: https://www.site.com/guidelines_for_protecting_and_storing_phi.pdf
+
+*To download these files*
+- You must be logged onto your site
+
+These results are the data that was requested under the authority of the Institutional Review Board.  The query resulting in this identified patient data is included at the end of this letter.  A copy of this letter is kept on file and is available to the IRB in the event of an audit.
+
+Thank you,
+
+The i2b2 Team 
+</Letter>
+  <Table>
+	<Filename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Procedure.csv</Filename>
+	<Query>SELECT distinct   to_char(a.PATIENT_NUM) as "i2b2 Patient Number"
+        ,m.patient_ide as ”MRN"
+       ,a.start_date as "Start Date"
+      ,a.end_date as "End Date"      
+      ,b.name_char as "Procedure Name"
+      ,b.concept_cd as "Procedure Code"
+      ,a.quantity_num as "Quantity"
+      ,a.location_cd as "Clinic"
+      ,case v.inout_cd  when 'O' then 'Outpatient' when 'I' then 'Inpatient' else 'Unknown' end as "Inpatient or Outpatient"
+     ,p.name_char as "Provider"
+     ,a.encounter_num as "Encounter Number"
+  FROM observation_fact  a 
+    inner  join concept_dimension b on 
+    a.concept_cd = b.concept_cd and
+   b.concept_path like '\i2b2\Procedures\%'
+   left outer join {{{DX}}} c on
+   a.patient_num = c.patient_num
+ left join provider_dimension p on
+   a.provider_id = p.provider_id
+ left join visit_dimension v on
+   a.encounter_num = v.encounter_num and
+   a.patient_num = v.patient_num
+left join patient_mapping m on
+   a.patient_num = m.patient_num</Query>
+      	<SeparatorCharacter>\t</SeparatorCharacter>
+  </Table>
+
+</ValueExporter>',null,null,null)
+;
+insert into QT_BREAKDOWN_PATH (NAME,VALUE,CREATE_DATE,UPDATE_DATE,USER_ID) values ('PATIENT_DIAGNOSIS_CSV','<?xml version="1.0"?>
+<ValueExporter>
+  <Version>3.02</Version>
+  <CreationDateTime>08/09/2024 12:00:00</CreationDateTime>
+  <RequesterEmailLetter>Your request on {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_ID}}}</RequesterEmailLetter>
+  <RequestLetter>This user {{{USER_NAME}}} in project {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_ID}}}.
+  </RequestLetter>
+  <LetterFilename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Readme.txt</LetterFilename>
+  <Letter>
+Results of the i2b2 request entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, are available.
+
+Important notes about your data:
+	- Total number of patients returned in your data request: {{{PATIENT_COUNT}}}
+	- i2b2 reviewer:
+	
+Only persons specifically authorized and selected (as listed at the top of this letter) can download these files. If additional user access is needed, please ensure the person is listed on your project IRB protocol and contact the i2b2 team.
+ 
+Specifically:
+	- Remove all PHI from computer, laptop, or mobile device after analysis is completed.
+	- Do NOT share PHI or PII with anyone who is not listed on the IRB protocol.
+
+Your guideline for the storage of Protected Health Information can be found at: https://www.site.com/guidelines_for_protecting_and_storing_phi.pdf
+
+*To download these files*
+- You must be logged onto your site
+
+These results are the data that was requested under the authority of the Institutional Review Board.  The query resulting in this identified patient data is included at the end of this letter.  A copy of this letter is kept on file and is available to the IRB in the event of an audit.
+
+Thank you,
+
+The i2b2 Team 
+</Letter>
+  <Table>
+	<Filename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Diagnosis.csv</Filename>
+	<Query>SELECT distinct   to_char(a.PATIENT_NUM) as "i2b2 Patient Number"
+        ,m.patient_ide as ”MRN"
+,a.start_date as "Start Date"
+      ,b.name_char as "Diagnosis Name"
+      ,b.concept_cd as "Diagnosis Code"
+      ,a.location_cd as "Clinic"
+      ,case v.inout_cd  when 'O' then 'Outpatient' when 'I' then 'Inpatient' else 'Unknown' end as "Inpatient or Outpatient"
+     ,p.name_char as "Provider"
+     ,a.encounter_num as "Encounter Number"
+  FROM observation_fact  a 
+    inner  join concept_dimension b on 
+    a.concept_cd = b.concept_cd and
+   b.concept_path like '\i2b2\Diagnoses\%'
+   left outer join {{{DX}}} c on
+   a.patient_num = c.patient_num
+ left join provider_dimension p on
+   a.provider_id = p.provider_id
+ left join visit_dimension v on
+   a.encounter_num = v.encounter_num and
+   a.patient_num = v.patient_num
+left join patient_mapping m on
+   a.patient_num = m.patient_num/Query>
+      	<SeparatorCharacter>\t</SeparatorCharacter>
+  </Table>
+
+</ValueExporter>',null,null,null)
+;
+insert into QT_BREAKDOWN_PATH (NAME,VALUE,CREATE_DATE,UPDATE_DATE,USER_ID) values ('PATIENT_LAB_CSV','<?xml version="1.0"?>
+<ValueExporter>
+  <Version>3.02</Version>
+  <CreationDateTime>08/09/2024 12:00:00</CreationDateTime>
+  <RequesterEmailLetter>Your request on {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_ID}}}</RequesterEmailLetter>
+  <RequestLetter>This user {{{USER_NAME}}} in project {{{PROJECT_ID}}} requested i2b2 request
+ entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, with the query master of {{{QUERY_ID}}}.
+  </RequestLetter>
+  <LetterFilename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Readme.txt</LetterFilename>
+  <Letter>
+Results of the i2b2 request entitled - "{{{QUERY_NAME}}}", submitted on {{{QUERY_STARTDATE}}}, are available.
+
+Important notes about your data:
+	- Total number of patients returned in your data request: {{{PATIENT_COUNT}}}
+	- i2b2 reviewer:
+	
+Only persons specifically authorized and selected (as listed at the top of this letter) can download these files. If additional user access is needed, please ensure the person is listed on your project IRB protocol and contact the i2b2 team.
+ 
+Specifically:
+	- Remove all PHI from computer, laptop, or mobile device after analysis is completed.
+	- Do NOT share PHI or PII with anyone who is not listed on the IRB protocol.
+
+Your guideline for the storage of Protected Health Information can be found at: https://www.site.com/guidelines_for_protecting_and_storing_phi.pdf
+
+*To download these files*
+- You must be logged onto your site
+
+These results are the data that was requested under the authority of the Institutional Review Board.  The query resulting in this identified patient data is included at the end of this letter.  A copy of this letter is kept on file and is available to the IRB in the event of an audit.
+
+Thank you,
+
+The i2b2 Team 
+</Letter>
+  <Table>
+	<Filename>/{{{USER_NAME}}}/{{{QUERY_MASTER_ID}}}/Lab.csv</Filename>
+	<Query>SELECT distinct   to_char(a.PATIENT_NUM) as "i2b2 Patient Number"
+        ,m.patient_ide as ”MRN"
+,a.start_date as "Start Date"
+      ,b.name_char as ”Lab Name"
+      ,case when a.valtype_cd = 'T' then tval_char else to_char(nval_num) end as "Lab Results"
+      ,b.concept_cd as ”Lab Code"
+      ,a.location_cd as "Clinic"
+      ,case v.inout_cd  when 'O' then 'Outpatient' when 'I' then 'Inpatient' else 'Unknown' end as "Inpatient or Outpatient"
+     ,p.name_char as "Provider"
+     ,a.encounter_num as "Encounter Number"
+  FROM observation_fact  a 
+    inner  join concept_dimension b on 
+    a.concept_cd = b.concept_cd and
+   b.concept_path like '\i2b2\Labtests\%'
+   left outer join {{{DX}}} c on
+   a.patient_num = c.patient_num
+ left join provider_dimension p on
+   a.provider_id = p.provider_id
+ left join visit_dimension v on
+   a.encounter_num = v.encounter_num and
+   a.patient_num = v.patient_num
+left join patient_mapping m on
+   a.patient_num = m.patient_num/Query>
       	<SeparatorCharacter>\t</SeparatorCharacter>
   </Table>
 
