aab2612ef18530b212663182da6df25652f528bd
reetametta <reetametta@gmail.com>
2026-06-04 12:10:37 -0400

    Update crc_NIH_BREAKDOWNS_sqlserver.sql

--- edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/act/scripts/sqlserver/crc_NIH_BREAKDOWNS_sqlserver.sql
+++ edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/act/scripts/sqlserver/crc_NIH_BREAKDOWNS_sqlserver.sql
@@ -1,35 +1,3 @@
-DELETE FROM QT_BREAKDOWN_PATH
-WHERE NAME = 'PATIENT_AGE_COUNT_XML';
-INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_AGE_COUNT_XML', N'select top 100
-(case
-when x.a=9 then ''90+ years old''
-else cast(x.a*10 as varchar(10))+''-''+cast(x.a*10+9 as varchar(10))+'' years old''
-end) patient_range, t.patient_count
-from (
-select 0 a
-union all select 1
-union all select 2
-union all select 3
-union all select 4
-union all select 5
-union all select 6
-union all select 7
-union all select 8
-union all select 9
-) x left outer join (
-select a, count(*) patient_count
-from (
-select (case when a>9 then 9 else a end) a
-from (
-select floor(AGE_IN_YEARS_NUM/10) a
-from {{{DATABASE_NAME}}}patient_dimension
-where patient_num in (select patient_num from {{{DX}}})
-) t
-) t
-where a between 0 and 9
-group by a
-) t on x.a=t.a
-order by x.a', N'2020-05-15 15:38:25.263', N'2020-05-19 13:15:03.103', null);
 INSERT INTO QT_QUERY_RESULT_TYPE
 (
     RESULT_TYPE_ID,
