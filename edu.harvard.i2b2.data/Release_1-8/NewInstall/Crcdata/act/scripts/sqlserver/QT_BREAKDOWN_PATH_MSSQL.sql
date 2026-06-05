59b1531673bdbf2c9a6c537e936a66246b0b059e
reetametta <reetametta@gmail.com>
2026-06-05 08:42:23 -0400

    Update SQL insert statements in QT_BREAKDOWN_PATH

--- edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/act/scripts/sqlserver/QT_BREAKDOWN_PATH_MSSQL.sql
+++ edu.harvard.i2b2.data/Release_1-8/NewInstall/Crcdata/act/scripts/sqlserver/QT_BREAKDOWN_PATH_MSSQL.sql
@@ -41,41 +41,6 @@ INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) V
 INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_CHARLSON_XML', N'\\ACT_RESEARCH\ACT\Research\Comorbidities\Charlson\CharlsonComorbidity\', N'2023-10-01 15:38:25.260', null, null);
 INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_COMMON_LAB_XML', N'\\ACT_COVID_V1\ACT\UMLS_C0031437\SNOMED_3947185011\UMLS_C0242656\LABS_OF_INTEREST\', N'2023-10-01 15:38:25.260', null, null);
 -- INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_NIH_ENROLLMENT_XML', N'\\ACT_RESEARCH\ACT\Research\NIH Enrollment\', N'2023-10-01 15:38:25.260', null, null);
--- PATIENT_NIH_ENROLLMENT_XML
--- Update the innermost subquery based on how you store sex, race, and ethnicity.
-INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_NIH_ENROLLMENT_XML', N'select r.name+''-''+s.name+''-''+e.name patient_range, sum(case when p.race_cd is not null then 1 else 0 end) patient_count
-from
-(
-select ''NI'' race_cd, ''No Race Info'' name
-union all select ''AS'', ''Asian''
-union all select ''B'', ''Black or African American''
-union all select ''H'', ''Native Hawaiian or Other Pacific Islander''
-union all select ''M'', ''Multiple race''
-union all select ''NA'', ''American Indian or Alaska Native''
-union all select ''W'', ''White''
-) r cross join (
-select ''NI'' sex_cd, ''No Sex Info'' name
-union all select ''F'', ''Female''
-union all select ''M'', ''Male''
-) s cross join (
-select ''NI'' ethnic_cd, ''No Ethnicity Info'' name
-union all select ''Y'', ''Hispanic''
-union all select ''N'', ''Not Hispanic''
-) e
-left outer join (
-select
-(case when race_cd in (''AS'',''B'',''H'',''M'',''NA'',''W'') then race_cd else ''NI'' end) race_cd,
-(case when sex_cd in (''F'',''M'') then sex_cd else ''NI'' end) sex_cd,
-(case when ethnic_cd in (''Y'',''N'') then ethnic_cd else ''NI'' end) ethnic_cd
-from (
-select race_cd, sex_cd, (case when race_cd=''Hispanic'' then ''Y'' else ''N'' end) ethnic_cd
-from {{{DATABASE_NAME}}}patient_dimension
-where patient_num in (select patient_num from {{{DX}}})
-) t
-) p
-on p.race_cd=r.race_cd and p.sex_cd=s.sex_cd and p.ethnic_cd=e.ethnic_cd
-group by r.name, s.name, e.name
-order by patient_count', N'2023-10-01 15:38:25.260', null, null);
 INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_ELIXHAUSER_XML', N'\\ACT_RESEARCH\ACT\Research\Comorbidities\Elixhauser\', N'2023-10-01 15:38:25.260', null, null);
 INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_TOP20MEDS_XML', N'select top 20 b.name_char as patient_range, count(distinct a.patient_num) as patient_count from {{{DATABASE_NAME}}}observation_fact a, {{{DATABASE_NAME}}}concept_dimension b, {{{DATABASE_NAME}}}#DX c where a.concept_cd = b.concept_cd and concept_path like ''\ACT\Medications\%'' and a.patient_num = c.patient_num   group by name_char order by patient_count desc', N'2023-12-13 12:05:11.767', null, null);
 INSERT INTO QT_BREAKDOWN_PATH (NAME, VALUE, CREATE_DATE, UPDATE_DATE, USER_ID) VALUES (N'PATIENT_TOP20DIAG_XML', N'select top 20 b.name_char as patient_range, count(distinct a.patient_num) as patient_count
