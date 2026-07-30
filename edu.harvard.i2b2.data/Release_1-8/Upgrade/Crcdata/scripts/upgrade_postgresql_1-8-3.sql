bafe56a76739a2a57b1f89d6b6864579afdedf52
Jeff Klann <jklann@gmail.com>
2026-07-30 15:53:06 -0400

    created upgrade scripts for visual_type

---
+++ edu.harvard.i2b2.data/Release_1-8/Upgrade/Crcdata/scripts/upgrade_postgresql_1-8-3.sql
@@ -0,0 +1,55 @@
+--==============================================================
+-- Database Script to upgrade CRC from 1.8.3 to 1.8.4
+--==============================================================
+
+ALTER TABLE QT_QUERY_RESULT_TYPE
+ADD VISUAL_TYPE TEXT
+;
+
+UPDATE QT_QUERY_RESULT_TYPE
+SET VISUAL_TYPE = '{
+    "COUNT": true
+  }'
+WHERE NAME = 'PATIENT_COUNT_XML'
+;
+
+UPDATE QT_QUERY_RESULT_TYPE
+SET VISUAL_TYPE = '{
+    "BARS": true,
+    "TABLE": true,
+    "DOWNLOAD": true
+  }'
+WHERE NAME IN (
+    'PATIENT_GENDER_COUNT_XML',
+    'PATIENT_VITALSTATUS_COUNT_XML',
+    'PATIENT_RACE_COUNT_XML',
+    'PATIENT_LOS_XML',
+    'PATIENT_INOUT_XML'
+)
+;
+
+UPDATE QT_QUERY_RESULT_TYPE
+SET VISUAL_TYPE = '{
+    "PIECHART": true,
+    "BARS": {
+      "maxLabelLength": 15
+    },
+    "TABLE": true,
+    "DOWNLOAD": true
+  }'
+WHERE NAME = 'PATIENT_AGE_COUNT_XML'
+;
+
+UPDATE QT_QUERY_RESULT_TYPE
+SET VISUAL_TYPE = '{
+    "BARS": true,
+    "TABLE": {
+      "forceInitialDisplay": true
+    },
+    "DOWNLOAD": true
+  }'
+WHERE NAME IN (
+    'PATIENT_TOP20MEDS_XML',
+    'PATIENT_TOP20DIAG_XML'
+)
+;
